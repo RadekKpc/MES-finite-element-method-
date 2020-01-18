749bb6a23690782e94f393a204a5772e8e1962e2
Radekk814 <radek1198@o2.pl>
2020-01-18 23:42:54 +0100

    Project alomst finished, test need

--- InputData.py
+++ InputData.py
@@ -1,11 +1,7 @@
 class InputData:
-    N = 5
+    N = 10
     startPoint = 0.0
     endPoint = 1.0
-    functionA = lambda x: x
-    functionB = lambda x: x
-    functionC = lambda x: x
-    functionF = lambda x: x
-    beta = 1.0
-    gamma = 1.0
-    dirchlett = 1.0
+    beta = 4
+    gamma = 2
+    Ur = 30
--- main.py
+++ main.py
@@ -3,31 +3,80 @@ from InputData import InputData
 import numpy as np
 import matplotlib.pyplot as plt
 from functools import partial
+import scipy.integrate as si
 
 data = InputData()
 N = data.N
-step = (data.endPoint-data.startPoint)/(N-1)
+
+#Exponential function: np.exp(x)
+#Logarithm function: np.log(x)
+functionA = lambda x: 9*np.exp(x)
+functionB = lambda x: 2*x + x*x + 2
+functionC = lambda x: 30*x + 4*np.log(x)
+functionF = lambda x: 4*x
+
 H = []
+rightSite = []
+leftSite = []
+step = (data.endPoint-data.startPoint)/(N-1)
 
-lambdas = [partial(lambda x: x, i) for i in range(3)]
-
-for i in range(0,N,1):
-    print( i*step)
-
-#Generowanie piramidek
+#Pyramids generation
 for i in range(0,N,1):
     H.append(partial(lambda i,x: (((1/(step*i - step*(i-1)))*x - step*(i-1)/(step*i - step*(i-1))) if (x < step*i and x > step*(i-1)) else (((step*(i+1)/(step*(i+1)-step*i))-x*(1/(step*(i+1)-step*i))) if (x >= step*i and x< step*(i+1)) else 0.0)),i))
 
-
-for f in H:
-    E1 = []
-    E1args = []
-    for j in np.arange(0.0,1.0,0.01):
-        E1.append(j)
-        E1args.append(functionDerived(f)(j))
-    plt.plot(E1,E1args)
-    plt.show()
+#shift shift
+shift =  lambda x: data.Ur*H[N-1](x)
 
 
+# Filling Right site
+for i in range(0,N-1,1):
+    rightSite.append(si.quad(functionMultiply(H[i],functionF),data.startPoint,data.endPoint)[0] - data.gamma*H[i](0))
+rightSite.append(data.Ur)
 
+# Filling Left Site
+for i in range(0,N-1,1):
+    row = []
+    for j in range(0,N,1):
+        row.append(
+            si.quad(functionMultiply(functionMultiply(functionDerived(H[i]),functionDerived(H[j])),functionA),data.startPoint,data.endPoint)[0]
+            +si.quad(functionMultiply(functionMultiply(functionDerived(H[i]),H[j]),functionB),data.startPoint,data.endPoint)[0]+
+            si.quad(functionMultiply(functionMultiply(H[i],H[j]),functionC),data.startPoint,data.endPoint)[0]
+            - data.beta*H[i](0)*H[j](0)
+        )
+    leftSite.append(row)
 
+row = []
+for j in range(0,N-1,1):
+    row.append(0)
+row.append(1)
+leftSite.append(row)
+
+# print("Equations: ")
+# print(leftSite)
+# print(rightSite)
+
+L = np.array(leftSite)
+R = np.array(rightSite)
+
+# print("Equations in np:")
+# print(L)
+# print(R)
+
+u = np.linalg.solve(L,R)
+# print(u)
+
+linearCombination = []
+for i in range(0,N,1):
+    linearCombination.append(functionMultiply(H[i],lambda x: u[i]))
+
+# Showing result
+E1 = []
+E1args = []
+for j in np.arange(0.0,1.0,0.01):
+    E1.append(j)
+    value = 0
+    for i in range(0,N,1):
+        value += linearCombination[i](j)
+    E1args.append(value + shift(j))
+plt.plot(E1,E1args)
+plt.show()
